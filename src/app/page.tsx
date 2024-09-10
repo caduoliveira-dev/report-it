0e24dc552c15077efcb704181af33f6e0041776b
Carlos Eduardo <ribeirocarlosoliveira@gmail.com>
2024-09-09 21:52:46 -0300

    primeiro commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,174 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar"
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+  CardFooter
+} from "@/components/ui/card"
+
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
+
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+import { 
+  AlignJustify,
+  CircleHelp
+
+} from "lucide-react"
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="relative">
+      <div className="flex justify-between p-4">
+        <div>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="outline"><AlignJustify /></Button>
+          </SheetTrigger>
+          <SheetContent side="left">
+            <SheetHeader>
+              <SheetTitle>
+              <Avatar>
+                <AvatarImage className="w-24" src="https://github.com/shadcn.png" alt="@shadcn" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+              </SheetTitle>
+              <SheetTitle>
+                Carlos Eduardo Ribeiro Oliveira
+              </SheetTitle>
+              <SheetDescription>
+                carlos@gmail.com
+              </SheetDescription>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+        </div>  
+        <div className="flex items-center justify-center">
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button
+                variant="outline"
+                size="icon"
+                className="overflow-hidden rounded-full"
+              >
+              <CircleHelp />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem>Ajuda</DropdownMenuItem>
+              <DropdownMenuItem>Comunicar um problema</DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button className="ml-4 bg-red-900 hover:bg-red-950">Iniciar Sessão</Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-auto pt-12">
+          <Tabs defaultValue="account" className="w-auto">
+            <TabsList className="grid w-full grid-cols-2">
+              <TabsTrigger value="entrar">Entrar</TabsTrigger>
+              <TabsTrigger value="cadastrar">Cadastrar</TabsTrigger>
+            </TabsList>
+            <TabsContent value="entrar">
+                <CardHeader>
+                  <CardTitle>Entrar</CardTitle>
+                  <CardDescription>
+                    Entre com seu email e senha.
+                  </CardDescription>
+                </CardHeader>
+                <CardContent className="space-y-2">
+                  <div className="space-y-1">
+                    <Label htmlFor="email">Email</Label>
+                    <Input id="email" type="email" placeholder="example@example.com" />
+                  </div>
+                  <div className="space-y-1">
+                    <Label htmlFor="password">Senha</Label>
+                    <Input id="password" type="password"/>
+                  </div>
+                </CardContent>
+                <CardFooter className="flex flex-col">
+                  <Button className="w-full">Entrar</Button>
+                  <Button className="w-full mt-2" variant="outline">
+                    Entrar com conta Google 
+                    <img className="ml-2" width="24" height="24" src="https://img.icons8.com/color/48/google-logo.png" alt="google-logo"/>
+                    </Button>
+                </CardFooter>
+            </TabsContent>
+            <TabsContent value="cadastrar">
+                <CardHeader>
+                  <CardTitle>Faça seu cadastro</CardTitle>
+                  <CardDescription>
+                    Preencha todos os campos.
+                  </CardDescription>
+                </CardHeader>
+                <CardContent className="space-y-2">
+                  <div className="flex justify-between">
+                    <div className="mr-4">
+                    <Label htmlFor="firstname">Nome</Label>
+                    <Input id="nome" type="text" />
+                    </div>
+                    <div>
+                    <Label htmlFor="new">Sobrenome</Label>
+                    <Input id="sobrenome" type="text" />
+                    </div>
+                  </div>
+                  <div className="space-y-1">
+                    <Label htmlFor="email">Email</Label>
+                    <Input id="email" type="email" placeholder="example@example.com" />
+                  </div>
+                  <div className="space-y-1">
+                    <Label htmlFor="senha">Senha</Label>
+                    <Input id="senha" type="password" />
+                  </div>
+                </CardContent>
+                <CardFooter>
+                  <Button className="w-full">Cadastrar</Button>
+                </CardFooter>
+            </TabsContent>
+          </Tabs>
+          </DialogContent>
+        </Dialog>
+        
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
