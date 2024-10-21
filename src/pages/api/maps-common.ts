7095f4aa76a0fd8aee1bfd8f71f13e85bdcaf96d
joao <joaopedro99_stylle@hotmail.com>
2024-10-21 19:39:49 -0300

    fix api do maps

--- src/pages/api/maps-common.ts
+++ src/pages/api/maps-common.ts
@@ -8,8 +8,8 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
-  let common = await fetch('https://maps.googleapis.com/maps-api-v3/api/js/58/4a/intl/pt_br/common.js').then(e => e.text());
-  common = common.replace(/(?=const\ a=\_\.cs\(\$r\().*?(?<=\)\;)/, 'const a = "https://geo-devrel-javascript-samples.web.app/";')
+  let common = await fetch('https://maps.googleapis.com/maps-api-v3/api/js/58/8/intl/pt_br/common.js').then(e => e.text());
+  common = common.replace(/(?=const\ a=\_\..s\().*?(?<=\)\;)/gm, 'const a = "https://geo-devrel-javascript-samples.web.app/";')
 
   res.status(200).write(common);
   res.end();
